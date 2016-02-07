20439b072ea0172e86ae32556ffc621b18ca8455
Simon  Flachs <Simon@Simons-MacBook-Air-4.local>
2016-02-07 16:49:12 +0100

    refac

--- analysis.py
+++ analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor, BaggingRegressor
-import FeatureSelector
-print(1)
+
+
 df_train = pd.read_csv("files/train.csv", encoding="ISO-8859-1")
 df_test = pd.read_csv("files/test.csv", encoding="ISO-8859-1")
 descriptions = pd.read_csv("files/product_descriptions.csv", encoding="ISO-8859-1")
@@ -20,13 +20,13 @@ def common_words(str1, str2):
 
 df = pd.concat((df_train, df_test), axis=0, ignore_index=True)
 df = pd.merge(df, descriptions, how='left', on='product_uid')
-print(2)
+
 df["word_in_title"] = df.apply(lambda row: common_words(row["search_term"], row["product_title"]), axis=1)
 df["word_in_description"] = df.apply(lambda row: common_words(row["search_term"], row["product_description"]), axis=1)
 
 train = df.iloc[:num_train]
 test = df.iloc[num_train:]
-print(3)
+
 predictors = ["word_in_description", "word_in_title"]
 
 rf = RandomForestRegressor(n_estimators=15, max_depth=6, random_state=0)
@@ -38,5 +38,3 @@ submission = pd.DataFrame({
         "id": test["id"],
         "relevance": predictions
     }).to_csv('submission.csv',index=False)
-
-#FeatureSelector.check(train, predictors, "relevance")
